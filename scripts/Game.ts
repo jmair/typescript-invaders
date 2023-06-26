89ffa1803448fb831392e15f5f85de8e05082c76
Jerimiah Mair <jerimiahmair@gmail.com>
2023-06-26 12:47:50 -0600

    isolate Game class methods and properties

--- scripts/Game.ts
+++ scripts/Game.ts
@@ -9,34 +9,34 @@ import Dialog from './Dialog.js';
 import Bombardment from './Bombardment.js';
 import PlayerLives from './PlayerLives.js';
 import Outpost from './Outpost.js';
-import Sprite from './Sprite.js';
 
 class Game {
+  #playerDead = false;
+  #gameStarted = false;
+  #gameOver = false;
+  #playerDeadTicks = 0;
+  #maxDeadTicks = 100;
+  #initialPlayerLives = 3;
+  #startText = 'Press [SPACE] to start a game.';
+  #gameOverText = 'Game Over!';
+  #tryAgainText = 'Press [SPACE] to try again.';
   #ctx: CanvasRenderingContext2D;
   #width: number;
   #height: number;
   #backgroundColor: string;
-  player: Player;
-  armada: Armada;
-  laser: Laser;
-  explosion: Explosion;
-  outpost: Outpost;
-  maxDeadTicks = 100;
-  readonly initialPlayerLives = 3;
-  bombardment: Bombardment;
-  score: Score;
-  playerLives: PlayerLives;
-  tryAgainDialog: Dialog;
-  startDialog: Dialog;
-  gameOverDialog: Dialog;
-  playerDead = false;
-  playerInput: Actions;
-  sprites: Sprite[] = [];
-  gameStarted = false;
-  gamePaused = false;
-  gameOver = false;
-  playerDeadTicks = 0;
-  playerCurrentLives: number;
+  #player: Player;
+  #armada: Armada;
+  #laser: Laser;
+  #explosion: Explosion;
+  #outpost: Outpost;
+  #bombardment: Bombardment;
+  #score: Score;
+  #playerLives: PlayerLives;
+  #tryAgainDialog: Dialog;
+  #startDialog: Dialog;
+  #gameOverDialog: Dialog;
+  #playerInput = defaultActions;
+  #playerCurrentLives: number;
 
   constructor(
     public canvas: HTMLCanvasElement,
@@ -52,154 +52,174 @@ class Game {
     this.#width = canvas.width || 1200;
     this.#height = canvas.height || 900;
     this.#backgroundColor = options.backgroundColor;
-    this.score = new Score(this.#ctx);
-    this.outpost = new Outpost(this.#ctx, this.canvas);
-    this.playerInput = defaultActions;
-    this.playerCurrentLives = this.initialPlayerLives;
+    this.#score = new Score(this.#ctx);
+    this.#outpost = new Outpost(this.#ctx, this.canvas);
+    this.#playerCurrentLives = this.#initialPlayerLives;
   }
 
-  init = async () => {
-    new Input(this._handleInput);
-    this.explosion = new Explosion(this.imageCache, this.#ctx, this.canvas);
-    this.laser = new Laser(this.imageCache, this.#ctx, this.outpost);
-    this.player = new Player(this.imageCache, this.#ctx, this.canvas);
-    this.armada = new Armada(
+  public init = async () => {
+    new Input(this.#handleInput);
+    this.#explosion = new Explosion(this.imageCache, this.#ctx, this.canvas);
+    this.#laser = new Laser(this.imageCache, this.#ctx, this.#outpost);
+    this.#player = new Player(this.imageCache, this.#ctx, this.canvas);
+    this.#armada = new Armada(
       this.imageCache,
       this.#ctx,
       this.canvas,
-      this._gameOver,
-      this.outpost.reset
+      this.#endGame,
+      this.#outpost.reset
     );
-    this.score = new Score(this.#ctx);
-    this.startDialog = new Dialog(this.#ctx, this.canvas, 24, [
-      'Press [SPACE] to start a game.',
+    this.#score = new Score(this.#ctx);
+    this.#startDialog = new Dialog(this.#ctx, this.canvas, 24, [
+      this.#startText,
     ]);
-    this.gameOverDialog = new Dialog(this.#ctx, this.canvas, 24, [
-      'Game Over!',
+    this.#gameOverDialog = new Dialog(this.#ctx, this.canvas, 24, [
+      this.#gameOverText,
     ]);
-    this.tryAgainDialog = new Dialog(
+    this.#tryAgainDialog = new Dialog(
       this.#ctx,
       this.canvas,
       14,
-      ['Press [SPACE] to try again.'],
+      [this.#tryAgainText],
       24
     );
-    this.bombardment = new Bombardment(
+    this.#bombardment = new Bombardment(
       this.imageCache,
       this.#ctx,
       this.canvas,
-      this.armada,
+      this.#armada,
       this.player,
-      this.killPlayer,
-      this.outpost
+      this.#killPlayer,
+      this.#outpost
     );
-    this.playerLives = new PlayerLives(this.imageCache, this.#ctx, this.canvas);
-    this.loop();
+    this.#playerLives = new PlayerLives(
+      this.imageCache,
+      this.#ctx,
+      this.canvas
+    );
+    this.#loop();
   };
 
-  _handleInput = (actions: Actions) => {
+  get armada() {
+    return this.#armada;
+  }
+
+  get outpost() {
+    return this.#outpost;
+  }
+
+  get player() {
+    return this.#player;
+  }
+
+  get score() {
+    return this.#score;
+  }
+
+  #handleInput = (actions: Actions) => {
     if (actions.FIRE) {
-      if (this.gameStarted && !this.playerDead) {
-        this.laser.fire();
+      if (this.#gameStarted && !this.#playerDead) {
+        this.#laser.fire();
       } else {
-        this.gameStarted = true;
-        this.score.reset();
+        this.#gameStarted = true;
+        this.#score.reset();
       }
     }
-    this.playerInput = actions;
+    this.#playerInput = actions;
   };
 
-  _gameOver = () => {
-    this.gameStarted = false;
-    this.gameOver = true;
-    this.playerCurrentLives = this.initialPlayerLives;
-    this.armada.reset();
-    this.bombardment.reset();
-    this.outpost.reset();
+  #endGame = () => {
+    this.#gameStarted = false;
+    this.#gameOver = true;
+    this.#playerCurrentLives = this.#initialPlayerLives;
+    this.#armada.reset();
+    this.#bombardment.reset();
+    this.#outpost.reset();
   };
 
-  _paintBg = () => {
+  #paintBg = () => {
     this.#ctx.clearRect(0, 0, this.#width, this.#height);
     this.#ctx.fillStyle = this.#backgroundColor;
     this.#ctx.fillRect(0, 0, this.#width, this.#height);
   };
 
-  _renderOverlay = () => {
+  #renderOverlay = () => {
     this.#ctx.fillStyle = 'rgba(50, 50, 50, 0.9)';
     this.#ctx.fillRect(0, 0, this.#width, this.#height);
   };
 
-  _playerDeadPause = () => {
-    this.playerDeadTicks += 1;
+  #playerDeadPause = () => {
+    this.#playerDeadTicks += 1;
 
-    if (this.playerDeadTicks > this.maxDeadTicks) {
-      this.playerDeadTicks = 0;
-      this.playerDead = false;
+    if (this.#playerDeadTicks > this.#maxDeadTicks) {
+      this.#playerDeadTicks = 0;
+      this.#playerDead = false;
       this.player.sprite.show();
     }
   };
 
-  _update = () => {
-    if (this.playerDead) {
-      if (this.playerCurrentLives <= 0) {
-        this._gameOver();
+  #update = () => {
+    if (this.#playerDead) {
+      if (this.#playerCurrentLives <= 0) {
+        this.#endGame();
       }
-      this._playerDeadPause();
+      this.#playerDeadPause();
     } else {
       const playerCannon = {
         x: this.player.position().x + this.player.width() / 2,
         y: this.player.position().y,
       };
 
-      this.laser.update(playerCannon, this.armada, this.explosion, this.score);
-      this.player.update(this.playerInput);
-      this.armada.update();
-      this.bombardment.update();
+      this.#laser.update(
+        playerCannon,
+        this.#armada,
+        this.#explosion,
+        this.#score
+      );
+      this.player.update(this.#playerInput);
+      this.#armada.update();
+      this.#bombardment.update();
     }
   };
 
-  _render = () => {
-    this._paintBg();
-    this.explosion.render();
-    this.laser.render();
+  #render = () => {
+    this.#paintBg();
+    this.#explosion.render();
+    this.#laser.render();
     this.player.render();
-    this.armada.render();
-    this.bombardment.render();
-    this.playerLives.render(this.playerCurrentLives);
-    this.outpost.render();
+    this.#armada.render();
+    this.#bombardment.render();
+    this.#playerLives.render(this.#playerCurrentLives);
+    this.#outpost.render();
 
-    if (!this.gameStarted) {
-      this._renderOverlay();
-      if (!this.gameOver) {
-        this.startDialog.render();
+    if (!this.#gameStarted) {
+      this.#renderOverlay();
+      if (!this.#gameOver) {
+        this.#startDialog.render();
       } else {
-        this.gameOverDialog.render();
-        this.tryAgainDialog.render();
+        this.#gameOverDialog.render();
+        this.#tryAgainDialog.render();
       }
     }
 
-    this.score.render();
+    this.#score.render();
   };
 
-  killPlayer = () => {
-    this.playerCurrentLives -= 1;
-    this.playerDead = true;
+  #killPlayer = () => {
+    this.#playerCurrentLives -= 1;
+    this.#playerDead = true;
     this.player.sprite.hide();
-    this.explosion.sprite().moveTo(this.player.sprite.position());
-    this.explosion.sprite().play();
+    this.#explosion.sprite().moveTo(this.player.sprite.position());
+    this.#explosion.sprite().play();
   };
 
-  pause = () => (this.gamePaused = true);
-
-  resume = () => (this.gamePaused = false);
-
-  loop = () => {
-    if (this.gameStarted) {
-      this._update();
+  #loop = () => {
+    if (this.#gameStarted) {
+      this.#update();
     }
-    this._render();
+    this.#render();
 
-    window.requestAnimationFrame(this.loop);
+    window.requestAnimationFrame(this.#loop);
   };
 }
 
