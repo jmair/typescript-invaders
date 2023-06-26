dabc6eb0a7ef481b3046dd925dac6a50dc61f566
Jerimiah Mair <jerimiahmair@gmail.com>
2023-06-26 14:21:23 -0600

    isolate Laser properties and methods

--- scripts/Laser.ts
+++ scripts/Laser.ts
@@ -9,59 +9,75 @@ import Armada from './Armada.js';
 import Base from './Base.js';
 
 class Laser {
-  outpost: Outpost;
-  config: SpriteConfig;
-  sprite: Sprite;
-  moving: boolean;
+  #outpost: Outpost;
+  #sprite: Sprite;
+  #moving: boolean;
 
   constructor(
     imageCache: ImageCache,
     ctx: CanvasRenderingContext2D,
     outpost: Outpost
   ) {
-    this.outpost = outpost;
-    this.config = {
+    this.#outpost = outpost;
+    const config: SpriteConfig = {
       startPosition: { x: -100, y: -100 },
       tag: 'laser',
       sheetSize: { w: 12, h: 24 },
       frameCount: 4,
       ticksPerFrame: 180,
     };
-    this.sprite = new Sprite(imageCache, ctx, this.config);
-    this.moving = false;
+
+    this.#sprite = new Sprite(imageCache, ctx, config);
+    this.#moving = false;
   }
 
-  fire = () => {
-    this.sprite.show();
-    this.moving = true;
+  public fire = () => {
+    this.#sprite.show();
+    this.#moving = true;
   };
 
-  moveTo = (position: Position) => {
-    this.sprite.moveTo(position);
+  public moveTo = (position: Position) => {
+    this.#sprite.moveTo(position);
   };
 
-  _moveOrResetToPlayer = (playersCannon: Position) => {
-    if (this.moving) {
-      this.sprite.move({ x: 0, y: -20 });
-    } else {
-      // if it's not moving reset it to the player's position
-      this.sprite.hide();
-      this.moveTo(playersCannon);
-    }
-  };
-
-  _checkIfOffScreen = () => {
-    if (this.sprite.position().y < 0) {
-      this.moving = false;
-    }
-  };
-
-  _checkArmadaCollision = (
+  public update = (
+    playersCannon: Position,
     armada: Armada,
     explosion: Explosion,
     score: Score
   ) => {
-    const laser = this.sprite.position();
+    this.#checkArmadaCollision(armada, explosion, score);
+    this.#checkIfOffScreen();
+    this.#moveOrResetToPlayer(playersCannon);
+    this.#checkForBaseCollision();
+  };
+
+  public render = () => {
+    this.#sprite.render();
+  };
+
+  #moveOrResetToPlayer = (playersCannon: Position) => {
+    if (this.#moving) {
+      this.#sprite.move({ x: 0, y: -20 });
+    } else {
+      // if it's not moving reset it to the player's position
+      this.#sprite.hide();
+      this.moveTo(playersCannon);
+    }
+  };
+
+  #checkIfOffScreen = () => {
+    if (this.#sprite.position().y < 0) {
+      this.#moving = false;
+    }
+  };
+
+  #checkArmadaCollision = (
+    armada: Armada,
+    explosion: Explosion,
+    score: Score
+  ) => {
+    const laser = this.#sprite.position();
     const shipSquare = 32;
 
     armada.positions.forEach((position, index) => {
@@ -74,7 +90,7 @@ class Laser {
         if (armada.ships[index].sprite.isVisible()) {
           // do not collide with hidden ships
           score.update(armada.ships[index].points);
-          this.moving = false;
+          this.#moving = false;
           armada.hideShip(index);
           explosion.sprite.moveTo(position);
           explosion.sprite.play();
@@ -83,40 +99,24 @@ class Laser {
     });
   };
 
-  _checkForBaseCollision = () => {
+  #checkForBaseCollision = () => {
     let collision = false;
-    this.outpost.bases.forEach((base: Base, i: number) => {
+    this.#outpost.bases.forEach((base: Base, i: number) => {
       base.blockLists.forEach((list, j) => {
         list.forEach((block, k) => {
-          const laserPosition = this.sprite.position();
+          const laserPosition = this.#sprite.position();
           const xDiff = Math.abs(block.x - laserPosition.x);
           const yDiff = block.y - laserPosition.y;
           const xAlign = xDiff < 5;
           const yAlign = yDiff > 0 && yDiff < 32;
           if (xAlign && yAlign) {
-            this.outpost.removeBlock(i, j, k);
+            this.#outpost.removeBlock(i, j, k);
             collision = true;
           }
         });
       });
     });
     return collision;
-  };
-
-  update = (
-    playersCannon: Position,
-    armada: Armada,
-    explosion: Explosion,
-    score: Score
-  ) => {
-    this._checkArmadaCollision(armada, explosion, score);
-    this._checkIfOffScreen();
-    this._moveOrResetToPlayer(playersCannon);
-    this._checkForBaseCollision();
-  };
-
-  render = () => {
-    this.sprite.render();
   };
 }
 
